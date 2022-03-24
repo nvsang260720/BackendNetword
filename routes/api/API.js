b1fd13ff1c11eb51be314f9ce882b2fd1ab65554
nvsang260720 <nvsang.18it2@vku.udn.vn>
2022-03-24 10:23:47 +0700

    edit api banner

--- routes/api/API.js
+++ routes/api/API.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const BannerController = require('../../controller/api/BannerController')
+// const BannerController = require('../../controller/Api/BannerController')
 const SearchController = require('../../controller/Api/SearchController')
 
-router.post('/add-banner', BannerController.addBanner)
+// router.post('/add-banner', BannerController.addBanner)
 
-router.get('/get-banner', BannerController.getBanner)
+// router.get('/get-banner', BannerController.getBanner)
 
 router.post('/search-key', SearchController.postKey)
 
